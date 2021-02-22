fe733e6928c47f34f975162b82f6216e88e1f558
venustar1228 <venustar-1228@outlook.com>
2021-02-22 15:38:33 +0530

    Updated migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
+const PlotXToken = artifacts.require("MockPLOT");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function(deployer, network, accounts) {
+  deployer.then(async () => {
+	  await deployer.deploy(Migrations);
+	  let deployPlotusToken = await deployer.deploy(PlotXToken, "PLOT", "PLOT", 18,accounts[0], accounts[0]);
+	  await deployPlotusToken.mint(accounts[0],"30000000000000000000000000");
+  });
 };
